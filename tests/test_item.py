65c9c600aec46ffd81e28c4cea0cc9aa5555882f
wanya2002 <wanya2002@yandex.ru>
2023-04-23 18:08:04 +0300

    commit1

--- tests/test_item.py
+++ tests/test_item.py
@@ -1,1 +1,16 @@
 """Здесь надо написать тесты с использованием pytest для модуля item."""
+from src.item import Item
+
+def test_calculate_total_price():
+      item = Item('samsung', 450, 14)
+      assert item.calculate_total_price() == 6300
+
+def test_apply_discount():
+      item = Item('nokia', 875, 5)
+      Item.pay_rate = 0.6
+      item.apply_discount()
+      assert item.price == 525
+
+
+
+
